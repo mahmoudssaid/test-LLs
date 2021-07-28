fd547e82212c8c63ed1c8d89bee64d353d9a34a2
mahmoudssaied <mahoudsaaid0@gmail.com>
2021-07-28 20:13:52 +0200

    S commit

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/new-H-LLS'));
+app.use(express.static(path.join('./dist/new-H-LLS')));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/new-H-LLS/'}),
+    res.sendFile(path.join('index.html', {root: 'dist/new-H-LLS/'})),
 );
 
 // Start the app by listening on the default Heroku port
